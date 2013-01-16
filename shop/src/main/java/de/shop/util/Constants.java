d2fe28c82faa74a10ecd55d8ed72f25add6fd58e
Philipp Dallmann <phili.dallmann@gmail.com>
2013-01-16 08:39:26 +0100

    util ergänzt

--- shop/src/main/java/de/shop/util/Constants.java
+++ shop/src/main/java/de/shop/util/Constants.java
@@ -7,6 +7,14 @@ public final class Constants {
 	public static final int INT_ANZ_ZIFFERN = 11;
 	public static final int LONG_ANZ_ZIFFERN = 20;
 	
+	// JNDI-NAME fuer UserTransaction
+	public static final String TRANSACTION_NAME = "java:jboss/UserTransaction";
+	
+	// REST
+	public static final String ARTIKELVERWALTUNG_NS = "urn:shop:artikelverwaltung";
+	public static final String BESTELLVERWALTUNG_NS = "urn:shop:bestellverwaltung";
+	public static final String KUNDENVERWALTUNG_NS = "urn:shop:kundenverwaltung";
+	
 	private Constants() {
 	}
 }
